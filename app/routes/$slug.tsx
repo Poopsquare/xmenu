84f64cf0f9195bfbb5459e59e1190721b996f6ce
pedroribeiro89 <pedroribeiro89@hotmail.com>
2022-02-11 21:58:16 -0300

    create routes

---
+++ app/routes/$slug.tsx
@@ -0,0 +1,8 @@
+
+export default function Menu() {
+    // const post = useLoaderData();
+    return (
+        // <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div>teste</div>
+    );
+}
