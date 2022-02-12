7544ab2dff238f8198fbf1353834f6586ea1bfbe
pedroribeiro89 <pedroribeiro89@hotmail.com>
2022-02-11 22:27:45 -0300

    mock loading menu data and simple page building but not styled

--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -1,5 +1,6 @@
 import {LoaderFunction, useLoaderData} from "remix";
 import {BANDECO} from "../../sample-menu-data/bandeco";
+import {Category, Item, Menu} from "../../types/menu";
 
 const mockLoadingMenu = new Promise(function(resolve) {
     setTimeout(resolve, 2000, BANDECO);
@@ -11,9 +12,25 @@ export const loader: LoaderFunction = async ({params}) => {
 }
 
 export default function Menu() {
-    const menu = useLoaderData();
-    console.log('menu', menu)
+    const menu = useLoaderData<Menu>();
     return (
-        <div>teste</div>
+        <article>
+            <h1>{menu.name}</h1>
+            {
+                menu.categories.map((category: Category) => (
+                    <section>
+                        <h2>{category.name}</h2>
+                        <ul>
+                            {
+                                category.items.map((item: Item) => item.description ?
+                                    (<li>{item.name} - {item.description} - {item.price / 100}</li>) :
+                                    (<li>{item.name} - {item.price / 100}</li>)
+                                )
+                            }
+                        </ul>
+                    </section>
+                ))
+            }
+        </article>
     );
 }
