5bee015655b7cd2bc3a2e4c3c5784d8058417bb4
Marcelo Risoli <celorisoli@gmail.com>
2022-07-18 23:12:42 +0200

    bump deps and fixes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <article>
       <h1>Menu Editor</h1>
-      <Link to={"bandeco"}>Click here to open Menu</Link>
+      <Link to={"m/bandeco"}>Click here to open Menu</Link>
     </article>
   );
 }
